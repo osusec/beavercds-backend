b75b3d6d85b9bf770ab0cc361f942fa159001fab
Robert Detjens <github@detjens.dev>
2025-09-27 12:25:39 -0700

    Fix result handling in init command runner
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- src/commands/init.rs
+++ src/commands/init.rs
@@ -1,22 +1,17 @@
-use simplelog::error;
+use anyhow::Result;
 use std::fs::File;
 use std::io::Write;
 use std::process::exit;
+use tracing::error;
 
 use crate::init::{self as init, templatize_init};
 use crate::{access_handlers::frontend, commands::deploy};
 
-pub fn run(_interactive: &bool, _blank: &bool) {
+pub fn run(_interactive: &bool, _blank: &bool) -> Result<()> {
     let options: init::InitVars;
 
     if *_interactive {
-        options = match init::interactive_init() {
-            Ok(t) => t,
-            Err(e) => {
-                error!("Error in init: {e}");
-                exit(1);
-            }
-        };
+        options = init::interactive_init()?;
     } else if *_blank {
         options = init::blank_init();
     } else {
@@ -24,22 +19,13 @@ pub fn run(_interactive: &bool, _blank: &bool) {
     }
 
     let configuration = templatize_init(options);
-    let mut f = match File::create("rcds.yaml") {
-        Ok(t) => t,
-        Err(e) => {
-            error!("Error in init: {e}");
-            exit(1);
-        }
-    };
-    match f.write_all(configuration.as_bytes()) {
-        Ok(_) => (),
-        Err(e) => {
-            error!("Error in init: {e}");
-            exit(1);
-        }
-    }
+
+    let mut f = File::create("rcds.yaml")?;
+    f.write_all(configuration.as_bytes())?;
 
     // Note about external-dns
     println!("Note: external-dns configuration settings will need to be provided in rcds.yaml after file creation, under the `profiles.name.dns` key.");
     println!("Reference: https://github.com/bitnami/charts/tree/main/bitnami/external-dns");
+
+    Ok(())
 }
