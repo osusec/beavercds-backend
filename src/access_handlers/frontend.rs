c6598666e9f4009268eb6561cf20c9c0f9b13cbb
Robert Detjens <github@detjens.dev>
2026-05-05 21:51:49 -0700

    Implement check-access for frontend
    Signed-off-by: Robert Detjens <github@detjens.dev>

--- src/access_handlers/frontend.rs
+++ src/access_handlers/frontend.rs
@@ -1,9 +1,43 @@
-use anyhow::{Error, Result};
+use anyhow::{bail, Context, Result};
+use serde::Deserialize;
+use tracing::debug;
+use ureq;
 
 use crate::configparser::{get_config, get_profile_config};
 
-/// frontend dashbard access checks
+#[derive(Deserialize, Debug)]
+/// Returned JSON object from frontend (`{"status":"ok"}`)
+struct CheckAccessResponse {
+    status: String,
+}
+
+/// frontend dashboard access checks
 pub fn check(profile_name: &str) -> Result<()> {
     let profile = get_profile_config(profile_name)?;
+
+    debug!("checking frontend access at {}", profile.frontend_url);
+
+    let resp = ureq::get(format!("{}/api/checkaccess", profile.frontend_url))
+        .header("Authorization", format!("Token {}", profile.frontend_token))
+        .call()
+        .context("could not reach frontend API")?;
+
+    if resp.status() != 200 {
+        bail!(
+            "frontend returned unexpected status code: {}",
+            resp.status()
+        );
+    }
+
+    // also check content to make sure we're hitting frontend API
+    let body: CheckAccessResponse = resp
+        .into_body()
+        .read_json()
+        .context("frontend response was not valid JSON")?;
+
+    if body.status != "ok" {
+        bail!("frontend returned unexpected status: {:?}", body);
+    }
+
     Ok(())
 }
